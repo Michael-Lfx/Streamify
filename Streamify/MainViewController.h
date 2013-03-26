7036db26c471a6f174f439dc6ba820fc8215c91a
Zuyet Awarmatik <francesc.zuyet@gmail.com>
2013-03-26 21:56:51 +0800

    Add SideBarViewController
    -Turn off autoresizeMask in BaseViewController.
    -Add images

---
+++ Streamify/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Streamify
+//
+//  Created by Zuyet Awarmatik on 26/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MainViewController : BaseViewController
+
+@end
