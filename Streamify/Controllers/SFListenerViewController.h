aa36c4e903fccb060d3adebfd20170641da2afe5
Minh Tu Le <thekingv2004@gmail.com>
2013-03-29 02:29:12 +0800

    Restructured ViewController files in project folder's hierachy.

---
+++ Streamify/Controllers/SFListenerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFListenerViewController.h
+//  Streamify
+//
+//  Created by Le Minh Tu on 3/29/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SFListenerViewController : BaseViewController
+
+@end
---
+++ Streamify/Controllers/SFListenerViewController.m
@@ -0,0 +1,38 @@
+//
+//  SFListenerViewController.m
+//  Streamify
+//
+//  Created by Le Minh Tu on 3/29/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "SFListenerViewController.h"
+
+@interface SFListenerViewController ()
+
+@end
+
+@implementation SFListenerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
