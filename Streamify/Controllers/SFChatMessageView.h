e3c35fa61b566ab87c1d89d9a91650f0f100ee84
Zuyet Awarmatik <francesc.zuyet@gmail.com>
2013-04-06 01:52:01 +0800

    ChatMessageView Skeleton

---
+++ Streamify/Controllers/SFChatMessageView.h
@@ -0,0 +1,13 @@
+//
+//  SFChatMessageView.h
+//  Streamify
+//
+//  Created by Zuyet Awarmatik on 6/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SFChatMessageView : BaseViewController
+
+@end
