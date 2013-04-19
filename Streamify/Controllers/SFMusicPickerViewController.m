c40cfdcc9da6d200a9d9777cd87a72de2205f477
Tony Le <lvtien.dev@gmail.com>
2013-04-20 03:34:23 +0800

    working audio exporter

--- Streamify/Controllers/SFMusicPickerViewController.m
+++ Streamify/Controllers/SFMusicPickerViewController.m
@@ -11,6 +11,7 @@
 #import "SFMusicPickerViewController.h"
 #import "SFSongTableViewCell.h"
 #import "SFSong.h"
+#import <CoreMedia/CoreMedia.h>
 
 @interface SFMusicPickerViewController ()
 
@@ -117,35 +118,119 @@
 
 -(void)mediaItemToData:(NSURL *)URL
 {
-    // Implement in your project the media item picker
+    NSString *EXPORT_NAME = @"exported.caf";
     
-    AVURLAsset *songAsset = [AVURLAsset URLAssetWithURL:URL options:nil];
+	AVURLAsset *songAsset = [AVURLAsset URLAssetWithURL:URL options:nil];
     
-    AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset: songAsset
-                                                                      presetName: AVAssetExportPresetPassthrough];
+	NSError *assetError = nil;
+	AVAssetReader *assetReader = [AVAssetReader assetReaderWithAsset:songAsset
+                                                                error:&assetError];
     
-//    NSLog(@"%@", exporter.supportedFileTypes);
-    exporter.outputFileType = @"com.apple.m4a-audio";
+	if (assetError) {
+		NSLog (@"error: %@", assetError);
+		return;
+	}
+	
+	AVAssetReaderOutput *assetReaderOutput = [AVAssetReaderAudioMixOutput
+                                               assetReaderAudioMixOutputWithAudioTracks:songAsset.tracks
+                                               audioSettings: nil];
+                                              
+	if (! [assetReader canAddOutput: assetReaderOutput]) {
+		NSLog (@"can't add reader output... die!");
+		return;
+	}
+	[assetReader addOutput: assetReaderOutput];
+	
+	NSArray *dirs = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *documentsDirectoryPath = [dirs objectAtIndex:0];
+	NSString *exportPath = [documentsDirectoryPath stringByAppendingPathComponent:EXPORT_NAME];
+	if ([[NSFileManager defaultManager] fileExistsAtPath:exportPath]) {
+		[[NSFileManager defaultManager] removeItemAtPath:exportPath error:nil];
+	}
+	NSURL *exportURL = [NSURL fileURLWithPath:exportPath];
+	AVAssetWriter *assetWriter = [AVAssetWriter assetWriterWithURL:exportURL
+                                                           fileType:AVFileTypeCoreAudioFormat
+                                                             error:&assetError];
+
+	if (assetError) {
+		NSLog (@"error: %@", assetError);
+		return;
+	}
+	AudioChannelLayout channelLayout;
+	memset(&channelLayout, 0, sizeof(AudioChannelLayout));
+	channelLayout.mChannelLayoutTag = kAudioChannelLayoutTag_Stereo;
+	NSDictionary *outputSettings = [NSDictionary dictionaryWithObjectsAndKeys:
+									[NSNumber numberWithInt:kAudioFormatLinearPCM], AVFormatIDKey,
+									[NSNumber numberWithFloat:44100.0], AVSampleRateKey,
+									[NSNumber numberWithInt:2], AVNumberOfChannelsKey,
+									[NSData dataWithBytes:&channelLayout length:sizeof(AudioChannelLayout)], AVChannelLayoutKey,
+									[NSNumber numberWithInt:16], AVLinearPCMBitDepthKey,
+									[NSNumber numberWithBool:NO], AVLinearPCMIsNonInterleaved,
+									[NSNumber numberWithBool:NO],AVLinearPCMIsFloatKey,
+									[NSNumber numberWithBool:NO], AVLinearPCMIsBigEndianKey,
+									nil];
+	AVAssetWriterInput *assetWriterInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeAudio
+                                                                              outputSettings:outputSettings];
     
-    NSArray *directionPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentsPath = [directionPaths objectAtIndex:0];
-    NSString *exportFile = [[NSString alloc] initWithString:[documentsPath stringByAppendingPathComponent:
-                            @"exported.caf"]];
+	if ([assetWriter canAddInput:assetWriterInput]) {
+		[assetWriter addInput:assetWriterInput];
+	} else {
+		NSLog (@"can't add asset writer input... die!");
+		return;
+	}
+	
+	assetWriterInput.expectsMediaDataInRealTime = NO;
     
-    NSURL *exportURL = [NSURL fileURLWithPath:exportFile] ;
-    exporter.outputURL = exportURL;
+	[assetWriter startWriting];
+	[assetReader startReading];
     
-    // do the export
-    // (completion handler block omitted)
-    NSLog(@"Start convertion");
-    [exporter exportAsynchronouslyWithCompletionHandler:
-     ^{
-//         NSData *data = [NSData dataWithContentsOfFile:exportFile];
+	AVAssetTrack *soundTrack = [songAsset.tracks objectAtIndex:0];
+	CMTime startTime = CMTimeMake (0, soundTrack.naturalTimeScale);
+	[assetWriter startSessionAtSourceTime: startTime];
+	
+	__block UInt64 convertedByteCount = 0;
+	
+	dispatch_queue_t mediaInputQueue = dispatch_queue_create("mediaInputQueue", NULL);
+	[assetWriterInput requestMediaDataWhenReadyOnQueue:mediaInputQueue
+											usingBlock: ^
+	 {
+		 // NSLog (@"top of block");
+		 while (assetWriterInput.readyForMoreMediaData) {
+             CMSampleBufferRef nextBuffer = [assetReaderOutput copyNextSampleBuffer];
+             if (nextBuffer) {
+                 // append buffer
+                 [assetWriterInput appendSampleBuffer: nextBuffer];
+                 //				NSLog (@"appended a buffer (%d bytes)",
+                 //					   CMSampleBufferGetTotalSampleSize (nextBuffer));
+                 convertedByteCount += CMSampleBufferGetTotalSampleSize (nextBuffer);
+                 // oops, no
+                 // sizeLabel.text = [NSString stringWithFormat: @"%ld bytes converted", convertedByteCount];
+                 
+                 NSNumber *convertedByteCountNumber = [NSNumber numberWithLong:convertedByteCount];
+//                 [self performSelectorOnMainThread:@selector(updateSizeLabel:)
+//                                        withObject:convertedByteCountNumber
+//                                     waitUntilDone:NO];
+             } else {
+                 // done!
+                 [assetWriterInput markAsFinished];
+                 [assetWriter finishWriting];
+                 [assetReader cancelReading];
+                 NSDictionary *outputFileAttributes = [[NSFileManager defaultManager]
+                                                       attributesOfItemAtPath:exportPath
+                                                       error:nil];
+                 NSLog (@"done. file size is %lld",
+					    [outputFileAttributes fileSize]);
+//                 NSNumber *doneFileSize = [NSNumber numberWithLong:[outputFileAttributes fileSize]];
+//                 [self performSelectorOnMainThread:@selector(updateCompletedSizeLabel:)
+//                                        withObject:doneFileSize
+//                                     waitUntilDone:NO];
+                 break;
+             }
+         }
          
-         // Do with data something
-         NSLog(@"Finished convertion");
-         
-     }];
+	 }];
+	NSLog (@"bottom of convertTapped:");
+
 }
 
 
